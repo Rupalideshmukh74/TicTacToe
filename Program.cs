330b36f15713309648faf1c13d2f535a50ff2f2b
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-01 09:48:46 +0530

    [Rupali]Feat:Initilized Board

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        void CheckingPosition()
+        {
+            char[] board = InitializeBoard();
+        }
+         public char[] InitializeBoard()
+        {
+            
+            char[] board = new char[10];
+
+           
+            for (int index = 1; index < board.Length; index++)
+            {
+                board[index] = ' ';
+            }
+            return board;
+        }
+    }
+
+}
---
+++ Tic_tac_toe_game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Tic_tac_toe_game
+    {
+    }
+}
