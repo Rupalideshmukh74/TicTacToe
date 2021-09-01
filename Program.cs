58ed31c7e66ee1f323ad6c520c7b65aa7e603e87
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-01 11:34:07 +0530

    [Rupali]Feat:User input

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ namespace TicTacToe
 {
     class Program
     {
+        public static string Computer;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe!");
             InitializeBoard();
+            ChooseLetter();
         }
        
-        static  public void  InitializeBoard()
+        static  void  InitializeBoard()
         {
             
             char[] board = new char[10];
@@ -21,7 +23,29 @@ namespace TicTacToe
                 board[1] = ' ';
             }
             
-        }
-    }
 
-}
+            }
+        public static void ChooseLetter()
+        {
+            Console.WriteLine("select 'X' or 'O': ");
+            char Player = char.Parse(Console.ReadLine());
+            if (Player == 'X')
+            {
+                Computer = "O";
+            }
+            else if(Player == 'O')
+            {
+                Computer = "X";
+            }
+            else
+            {
+                Console.WriteLine("wrong input");
+            }
+        }
+
+    }
+ }
+
+    
+
+
